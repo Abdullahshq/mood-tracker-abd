088c7eef1053a747c70f999b8d1efe95f2b685a2
b-pieta <bozena.pieta95@gmail.com>
2023-11-20 21:41:26 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors());
+// app.use(bodyParser.json());
+
+
+// app.get('/', (req, res) => {
+//   res.send('Welcome to the Mood Tracker API!');
+// });
+
+
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}.`);
+// });
+
+console.log("Hello world")
