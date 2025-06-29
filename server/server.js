dab333e9e35f4bc657a6031d6bd88696ae19563e
Abdullahshq <abdullahshabbir787@gmail.com>
2025-06-29 12:16:25 +0500

    fix 15

--- server/server.js
+++ server/server.js
@@ -46,11 +46,11 @@ const startServer = async () => {
 
     // Serve frontend in production
     if (process.env.NODE_ENV === 'production') {
-        app.use(express.static(path.join(__dirname, '../client/mood-tracker/build')));
+        app.use(express.static(path.join(__dirname, 'public')));
 
-        app.get('*', (req, res) => 
-            res.sendFile(path.resolve(__dirname, '../', 'client', 'mood-tracker', 'build', 'index.html'))
-        );
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, 'public', 'index.html'));
+        });
     } else {
         app.get('/', (req, res) => {
             res.send('API is running...');
